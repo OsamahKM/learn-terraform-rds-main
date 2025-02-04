11cc303c1ec519868aa1d45d1ef43382c57daa93
Osamah <okadumm@gmail.com>
2025-02-04 11:29:27 +0100

    sensitive = false

--- outputs.tf
+++ outputs.tf
@@ -4,18 +4,18 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.education.address
-  sensitive   = true
+  sensitive   = false
 }
 
 output "rds_port" {
   description = "RDS instance port"
   value       = aws_db_instance.education.port
-  sensitive   = true
+  sensitive   = false
 }
 
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.education.username
-  sensitive   = true
+  sensitive   = false
 }
 
